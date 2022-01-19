8b03461d0a563d8502ff7ecc643348cade68917f
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-01-18 19:56:20 -0500

    found poles, zeros of H

--- lab01-polynomials/polynomials_m1.m
+++ lab01-polynomials/polynomials_m1.m
@@ -1,12 +1,17 @@
-% https://github.com/lduran2/ece3413_classical_control_systems/lab01/polynomials_m1.m
+% Canonical https://github.com/lduran2/
+%   ece3413_classical_control_systems/
+%   lab01-polynomials/polynomials_m1.m
 %
 % Finds the roots of various polynomials.
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-01-18t12:13R
+% When      : 2022-01-18t19:54R
 % For       : ECE 3413
-% Version   : 1.0.0
+% Version   : 1.1.0
 %
 % CHANGELOG :
+%   v1.1.0 - 2022-01-18t19:54R
+%       found poles, zeros of H (part 2)
+%
 %   v1.0.0 - 2022-01-18t12:13R
 %       part 1 completed
 
@@ -21,14 +26,14 @@ DEG_PER_RAD = 180/pi;
 disp('Part I')
 
 % Given the polynomial
-% P(s) = (s^3 + 7s^2 + 10s + 9)(s^4 - 3s^3 + 6s^2 + 2s + 1),
+% P(s) := (s^3 + 7s^2 + 10s + 9)(s^4 - 3s^3 + 6s^2 + 2s + 1),
 
 % the factors are
-%             4  3  2  1  0
-Pfactors = [  0  1  7 10  9;
-              1 -3  6  2  1 ]
+% x-order :   4  3  2  1  0
+B =        [     1  7 10  9 ]
+A =        [  1 -3  6  2  1 ]
 % We convolve to find polynomial P(s).
-P = conv(Pfactors(1,:), Pfactors(2,:))
+P = conv(B, A)
 
 % The roots are
 s = roots(P);
@@ -38,5 +43,19 @@ ph_deg = (angle(s) * DEG_PER_RAD);
 
 % display roots and angles in a table
 table(s, ph_deg)
+
+%% Part 2
+
+disp('Part II')
+
+% Given the transfer function
+% H(s) := \frac{s^3 + 7s^2 + 10s + 9}{s^4 - 3s^3 + 6s^2 + 2s + 1},
+% (Note that these are the parts as for the polinomial.)
+H = tf(B, A)
+
+% Find the poles (roots of the denominator).
+polesH = roots(cell2mat(H.Denominator))
+% Find the zeros (roots of the numerator).
+zerosH = roots(cell2mat(H.Numerator))
 
 disp('Done.')
