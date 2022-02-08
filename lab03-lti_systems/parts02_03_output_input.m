01e41a908da0ee8c4027509c3e8dee5db6d52101
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-08 00:09:49 -0500

    [lab03] plotting data vs time

--- lab03-lti_systems/parts02_03_output_input.m
+++ lab03-lti_systems/parts02_03_output_input.m
@@ -1,5 +1,12 @@
 % Part II
-% loading time and data from 'out.mat'
+% load time and data from 'out.mat'
 data = load('out.mat');
 t = data.ans.Time;
 y = data.ans.Data;
+
+% plot data vs time
+figure
+plot(t,y)
+title('step response, h')
+xlabel('time, t [s]')
+ylabel('conductance, h(t) [\Omega^{-1}]')
