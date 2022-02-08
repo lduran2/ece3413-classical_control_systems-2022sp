535a90330e62d4c695d1d842c3132ba114b42f91
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-08 11:55:09 -0500

    [lab0405] plotted each step response for G1(s;a)

--- lab0405/stepinfo_m1.m
+++ lab0405/stepinfo_m1.m
@@ -2,24 +2,36 @@
 %
 % Finds the roots of various polynomials.
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-02-08t11:25R
+% When      : 2022-02-08t11:39R
 % For       : ECE 3413
 % Version   : 1.0.0
 %
 % CHANGELOG :
-%   v1.0.0 - 2022-01-18t12:13R
-%       
+%   v1.0.0 - 2022-02-08t11:39R
+%       found stepinfo parameters for G1(s; a)
 
-for a = 1:4
+% for a in (1..4)
+as = 1:4
+for a=as
     % display a
-    a
+    disp(join(['%%%% a =' string(a) '%%%%']))
     % G1(s; a) := a/(s + a).
     B = [a]
     A = [1 a]
     G1_s = tf(B, A)
+
     % display settling time, rise time and peak time for G1(s; a)
     G1_s_step = stepinfo(G1_s);
     SettlingTime = G1_s_step.SettlingTime
     RiseTime = G1_s_step.RiseTime
     PeakTime = G1_s_step.PeakTime
+
+    % find the step response
+    [y, t] = step(G1_s);
+    % plot the result
+    figure
+    plot(y, t)
+    title(join(['Step response of G_1 for a =' string(a)]))
+    xlabel('time, t [s]')
+    ylabel('amplitude, G_1(s; a)')
 end % for a
