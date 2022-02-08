b0f161b80e54bc28b335df00df19488b3132ff1a
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-08 00:18:25 -0500

    [lab03] part 2 for output in script

--- lab03-lti_systems/parts01_02_output_input.m
+++ lab03-lti_systems/parts01_02_output_input.m
@@ -1,14 +1,20 @@
-%% Part II
+%% Part I. Step Response
 % load time and data from 'out.mat'
-data = load('out.mat');
-t = data.ans.Time;
-y = data.ans.Data;
+data = load('out.mat')
+t1 = data.ans.Time;
+y2 = data.ans.Data;
 
 % plot data vs time
 figure
-plot(t,y)
+plot(t1,y2)
 title('step response, h')
 xlabel('time, t [s]')
 ylabel('conductance, h(t) [\Omega^{-1}]')
 
-%% Part III
+%% Part II. Sinusoidal Response
+t2 = 0:0.001:10;
+y2 = 0.5*sin(t2) + 0.3;
+
+% construct timeseries to save
+ts2 = timeseries(t2, y2)
+save input.mat -v7.3 ts2
