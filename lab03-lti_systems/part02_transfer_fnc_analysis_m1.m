a922c0a5be28d78fb6cac3ade9eee48dc98f0839
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-12 14:12:21 -0500

    [lab03] generated transfer function

--- lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
+++ lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
@@ -1,13 +1,22 @@
 % Canonical https://github.com/lduran2/ece3413_classical_control_systems/lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
 % Automates simultion of various transfer functions
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-02-12t14:07R
+% When      : 2022-02-12t14:11R
 % For       : ECE 3413
-% Version   : 0.0.0
+% Version   : 1.0.0
 %
 % CHANGELOG :
+%   v1.0.0 - 2022-02-12t14:11R
+%       generated transfer function
+%
 %   v0.0.0 - 2022-02-12t14:07R
 %       hello world
 
+%% transfer function
+B = [2];        % the numerator
+A = [1 5 9];    % the denominator
+
+Ga_s = tf(B,A)
+
 % display finished
-disp('Hello world.')
+disp('Done.')
