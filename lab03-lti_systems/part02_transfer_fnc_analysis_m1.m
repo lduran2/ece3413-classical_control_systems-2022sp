4dfc283100c8d27b433e8305e5a7e326f299ace8
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-12 14:44:04 -0500

    [lab03] create simulation name

--- lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
+++ lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
@@ -1,21 +1,38 @@
 % Canonical https://github.com/lduran2/ece3413_classical_control_systems/lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
 % Automates simultion of various transfer functions
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-02-12t14:11R
+% When      : 2022-02-12t14:43R
 % For       : ECE 3413
-% Version   : 1.0.0
+% Version   : 1.1.0
 %
 % CHANGELOG :
+%   v1.0.0 - 2022-02-12t14:43R
+%       create the simulation name
+%
 %   v1.0.0 - 2022-02-12t14:11R
 %       generated transfer function
 %
 %   v0.0.0 - 2022-02-12t14:07R
 %       hello world
 
+%% constants
+tsim = 10.0; % [s] simulation time
+
+%% name of the simulation
+word_delim = '_';           % part delimiter in filenames
+sim_suffix = 'slx1.slx';    % common suffix for simulation files
+% split words by delimiter
+source_words = split(mfilename, word_delim)
+% remove the last word and rejoin by '_'
+source_base_cell = join({source_words{1:end-1}}, word_delim)
+% break out of cell and add simulation suffix
+sim_source = [ source_base_cell{:} word_delim sim_suffix ]
+
 %% transfer function
 B = [2];        % the numerator
 A = [1 5 9];    % the denominator
 
+% generate the transfer function
 Ga_s = tf(B,A)
 
 % display finished
