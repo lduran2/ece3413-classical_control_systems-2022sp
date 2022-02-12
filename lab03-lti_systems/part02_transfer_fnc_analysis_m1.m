b3f5a89698345095dfb1cc5363ebc1cf2d951e25
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-12 14:08:41 -0500

    [lab03] started part 2 script

---
+++ lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
@@ -0,0 +1,13 @@
+% Canonical https://github.com/lduran2/ece3413_classical_control_systems/lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
+% Automates simultion of various transfer functions
+% By        : Leomar Duran <https://github.com/lduran2>
+% When      : 2022-02-12t14:07R
+% For       : ECE 3413
+% Version   : 0.0.0
+%
+% CHANGELOG :
+%   v0.0.0 - 2022-02-12t14:07R
+%       hello world
+
+% display finished
+disp('Hello world.')
