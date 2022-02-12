0ceed65f337817691d1a30b308bc3ee5f706fde5
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-12 14:45:36 -0500

    [lab03] running simulation

--- lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
+++ lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
@@ -1,12 +1,15 @@
 % Canonical https://github.com/lduran2/ece3413_classical_control_systems/lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
 % Automates simultion of various transfer functions
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-02-12t14:43R
+% When      : 2022-02-12t14:45R
 % For       : ECE 3413
-% Version   : 1.1.0
+% Version   : 1.1.1
 %
 % CHANGELOG :
-%   v1.0.0 - 2022-02-12t14:43R
+%   v1.1.1 - 2022-02-12t14:45R
+%       running simulation
+%
+%   v1.1.0 - 2022-02-12t14:43R
 %       create the simulation name
 %
 %   v1.0.0 - 2022-02-12t14:11R
@@ -34,6 +37,9 @@ A = [1 5 9];    % the denominator
 
 % generate the transfer function
 Ga_s = tf(B,A)
+
+% run the simulation
+sim(sim_source)
 
 % display finished
 disp('Done.')
