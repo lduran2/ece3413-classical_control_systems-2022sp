32750b8d09a04481c662050acd7ad876baf94463
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-12 17:01:34 -0500

    closing tgt, scope; fig directory

--- lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
+++ lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
@@ -1,11 +1,14 @@
 % Canonical : https://github.com/lduran2/ece3413_classical_control_systems/lab03-lti_systems/part02_transfer_fnc_analysis_m1.m
 % Automates simultion of various transfer functions
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-02-12t16:45R
+% When      : 2022-02-12t17:01R
 % For       : ECE 3413
-% Version   : 1.1.3
+% Version   : 1.1.4
 %
 % CHANGELOG :
+%   v1.1.4 - 2022-02-12t17:01R
+%       closing the target and scope
+%
 %   v1.1.3 - 2022-02-12t16:45R
 %       copying the scope
 %
@@ -34,6 +37,9 @@ FILE_SUFFIX = '.slx';       % suffix for the simulation files
 SYS_DELIM = '/';            % delimiter for block levels
 N_SCOPE = 'Scope';          % the name of the scope
 
+%% output constants
+FIG_DIR = '../doc/lab03/fig';   % directory to where figures are saved
+
 %% name of the simulation
 % split words by delimiter
 source_words = split(mfilename, WORD_DELIM)
@@ -55,7 +61,8 @@ out = sim(sim_source)
 
 %% save the scope image
 % open the scope
-open_system([ sim_name SYS_DELIM N_SCOPE ])
+nScopeSys = [ sim_name SYS_DELIM N_SCOPE ]
+open_system(nScopeSys)
 % get the scope handle
 hScope = findall(0, 'Name', N_SCOPE)
 % get a handle for the source plot (opened by `open_system`)
@@ -67,7 +74,13 @@ set(hTgt, 'InvertHardCopy', 'off')
 % copy to target for saving
 copyobj(hSrc, hTgt)
 % save the result
-saveas(hTgt, 'figure/Ga_s.png')
+mkdir('fig')
+saveas(hTgt, [ FIG_DIR '/Ga_s.png' ])
+% close the target
+close(hTgt)
+
+% close the scope
+close_system(nScopeSys)
 
 %% get the data and time
 y = out.simout.data;
