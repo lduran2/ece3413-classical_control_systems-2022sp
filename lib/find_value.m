676d4e7737a2198cde832dc06712bb0030392d6c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-02-12 19:41:09 -0500

    use offset for `find_ceil`, fixed echos

--- lib/find_value.m
+++ lib/find_value.m
@@ -1,13 +1,17 @@
 % Canonical : https://github.com/lduran2/ece3413_classical_control_systems/lib/find_value.m
-% Finds and estimates a value in a vector
+% Functions for finding an expected value in a vector.
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-02-12t18:54R
+% When      : 2022-02-12t19:40R
 % For       : ECE 3413
-% Version   : 1.2.4
+% Version   : 1.2.5
 %
 % CHANGELOG :
+%   v1.2.5 - 2022-02-12t19:40R
+%       use offset for `find_ceil`
+%       fixed echos
+%
 %   v1.2.4 - 2022-02-12t17:06R
-%       abstracted `find_ceil` 
+%       abstracted `find_ceil` and `estimate_by_diff` from `lab03`
 %
 %   v1.2.3 - 2022-02-12t18:29R
 %       corrected rise time by difference
@@ -23,7 +27,7 @@
 % given key.
 function index = find_ceil(v, off, N, key)
     % for each sample
-    for k=1:N
+    for k=off:N
         % stop if 10%ile
         if (v(k) >= key)
             break
@@ -42,7 +46,7 @@ function est = estimate_by_diff(op_expected, op, index, ip_measured, delta_ip)
     % delta of input
     delta_op = (op(index) - op(index - 1));
     % difference at index from expected
-    op_diff = (op_expected - op(index))
+    op_diff = (op_expected - op(index));
     % use (y - y0) = dy/dx*(x - x0)
     est = (delta_ip/delta_op)*(op_diff) + ip_measured;
 end % function estimate_by_diff(v, index, op_expected, ip_measured)
