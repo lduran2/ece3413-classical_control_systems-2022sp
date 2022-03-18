3b28f035fd217618f4c3466f8fca41ac8050407c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-18 13:38:55 -0400

    [lab0405] single script for all parameters

---
+++ lab0405-time_responses/time_response_params_m1.m
@@ -0,0 +1,19 @@
+%%
+% Canonical : https://github.com/lduran2/ece3413_classical_control_systems/lab0405-time_responses/time_response_params_m1.m
+% Clears workspace and runs the scripts for parameters for the Time
+% Response model.
+% By        : Leomar Duran <https://github.com/lduran2>
+% When      : 2022-03-18t13:37R
+% For       : ECE 3413
+% Version   : 1.0.0
+%
+% CHANGELOG :
+%   v1.0.0 - 2022-03-18t13:37R
+%       gets the parameters for part 01
+
+clear
+
+%% parameters for part 01
+part0102_reals_m1
+part0103_imags_m1
+part0104_nat_freqs_m1
