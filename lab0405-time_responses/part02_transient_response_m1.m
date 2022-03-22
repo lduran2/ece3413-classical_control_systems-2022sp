dbe4b26413144f1e0b11d681b1f973fbc5cefab4
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-22 16:22:52 -0400

    [lab0405] looping through all parameters for all systems

--- lab0405-time_responses/part02_transient_response_m1.m
+++ lab0405-time_responses/part02_transient_response_m1.m
@@ -2,11 +2,14 @@
 % Canonical : https://github.com/lduran2/ece3413_classical_control_systems/lab0405-time_responses/part02_transient_response_m1.m
 % The transient response plots and pole-zero plots of various systems.
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-03-22t15:45Q
+% When      : 2022-03-22t16:21Q
 % For       : ECE 3413
-% Version   : 1.3.0
+% Version   : 1.4.0
 %
 % CHANGELOG :
+%   v1.4.0 - 2022-03-22t16:21Q
+%       looping through all parameters for all systems
+%
 %   v1.3.0 - 2022-03-22t15:45Q
 %       added G4(s; 3, 3.01), G5(s; 30, 30.01)
 %
@@ -21,26 +24,45 @@
 
 clear
 
+%% the range of the system subscripts
+sys_range = 3:5
+
 %% G3(s; p, z)
 % function handle for producing system G3(s; p)
-G3 = @(p, z) zpk(z, [roots([1 4 25]); p], 25)
+G{3} = @(p, z) zpk(z, [roots([1 4 25]); p], 25)
+
+% poles and zeroes for G3
+the_poles{3,:} = {-200, -20, -10, -2,   [],  [],  [], [], []}
+the_zeros{3,:} = {  [],  [],  [], [], -200, -50, -20, -5, -2}
+[~, N_PARAMS{3}] = size(the_poles{3,:})
 
 %% G4(s; a, b) and G5(s; a, b) as G45(s; a, b; c)
 G45 = @(c) (@(a, b) zpk([-a], [-b; roots([1 c])], (c(end)*b/a)))
-G4 = G45([4 25])
-G5 = G45([40 2500])
 
-% poles for G3
-G3poles = {-200, -20, -10, -2,   [],  [],  [], [], []}
-G3zeros = {  [],  [],  [], [], -200, -50, -20, -5, -2}
-[~, N_G3_PARAMS] = size(G3poles)
+% G4
+G{4} = G45([4 25])
+% poles and zeroes
+the_poles{4,:} = { 3.01, 3.1, 3.3, 3.5, 4.0 }
+the_zero{4} = 3
+[~, N_PARAMS{4}] = size(the_poles{4,:})
+the_zeros{4,:} = num2cell( the_zero{4}*ones(1, N_PARAMS{4}) )
 
-% for each pole
-for i_param=1:N_G3_PARAMS
-    G3(G3poles{i_param}, G3zeros{i_param})
-end % for for i_poles
+% G5
+G{5} = G45([40 2500])
+% poles and zeroes
+the_poles{5,:} = { 30.01, 30.1, 30.5, 31, 35, 40 }
+the_zero{5} = 30
+[~, N_PARAMS{5}] = size(the_poles{5,:})
+the_zeros{5,:} = num2cell( the_zero{5}*ones(1, N_PARAMS{5}) )
 
-G4(3, 3.01)
-G5(30, 30.01)
+%% analysis
+% for each system
+for i_sys = sys_range
+    % for each pole
+    for i_param=1:N_PARAMS{i_sys}
+        G{i_sys}(the_poles{i_sys}{i_param}, the_zeros{i_sys}{i_param})
+    end % for for i_param
+end % for i_sys
 
+%% report done
 disp('Done.')
