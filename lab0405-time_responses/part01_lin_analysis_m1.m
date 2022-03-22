7c6e2f25309a6d92653be54853241a39ba2b15b1
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-22 12:30:41 -0400

    [lab0405] pole table

--- lab0405-time_responses/part01_lin_analysis_m1.m
+++ lab0405-time_responses/part01_lin_analysis_m1.m
@@ -2,11 +2,14 @@
 % Canonical : https://github.com/lduran2/ece3413_classical_control_systems/lab0405-time_responses/part01_lin_analysis_m1.m
 % Displays each function's poles and each group of linear system analyses.
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-03-22t11:25Q
+% When      : 2022-03-22t12:30Q
 % For       : ECE 3413
-% Version   : 1.0.3
+% Version   : 1.0.4
 %
 % CHANGELOG :
+%   v1.0.4 - 2022-03-22t12:30Q
+%       pole table
+%
 %   v1.0.3 - 2022-03-22t11:25Q
 %       poles of 2nd order, skip part menu
 %
@@ -23,7 +26,7 @@ clear
 
 %% debug modes
 global DO_SKIP_PART_MENU
-DO_SKIP_PART_MENU = false
+DO_SKIP_PART_MENU = true
 
 %% constants
 global PART_SENTINEL
@@ -46,25 +49,43 @@ for a=1:n_tfG(i_part)
 end % for a=1:n_tfG(i_part)
 
 %% create modified transfer functions for part I 02-04 analysis
+N_POLES_0102_0104_COLS = 2*max(n_tfG(2:N_PARTS))
+poles_0102_0104_cols = cell(1, N_POLES_0102_0104_COLS);
+poles_0102_0104 = cell(N_PARTS-1, N_POLES_0102_0104_COLS);
 % for each part
 i_part_tf = 0;
 for i_part=2:N_PARTS
-    for i_tf=2:n_tfG(i_part)
-        i_part_tf = i_part_tf + 1
+    for i_tf=1:n_tfG(i_part)
+        % if not tf/col 1
+        if (i_tf > 1)
+            % increment count
+            i_part_tf = i_part_tf + 1
+            part_tf_no = i_part_tf
+            % build the transfer function
+            tfG{i_part, i_tf} = tf( ...
+                G_b(i_part, i_tf), ...
+                [1 G_a(i_part, i_tf), G_b(i_part, i_tf)] ...
+            );
+        else
+            % otherwise, just label as #1
+            part_tf_no = 1
+        end % if (i_tf > 1) ||
         tfG_name = join( ...
             ['transfer function 2' ...
-                char('`' + i_part_tf) ...
+                char('`' + part_tf_no) ...
             ] ...
             , '' ...
-        );
-        disp(tfG_name)
-        % build the transfer function
-        tfG{i_part, i_tf} = tf( ...
-            G_b(i_part, i_tf), ...
-            [1 G_a(i_part, i_tf), G_b(i_part, i_tf)] ...
-        );
+        )
         % retrieve poles
-        zpk(tfG{i_part, i_tf}).P
+        tf_poles = zpk(tfG{i_part, i_tf}).P{:}
+        pole_row = (i_part - 1)
+        pole_col1 = (2*i_tf - 1)
+        poles_0102_0104{pole_row, pole_col1    } = tf_poles(1);
+        poles_0102_0104{pole_row, pole_col1 + 1} = tf_poles(2);
+        % show graph A only once
+        if ((i_tf == 1) && (i_part > 2))
+            continue
+        end %
         % plot poles and zeros
         fig = figure;
         pzmap(tfG{i_part, i_tf})
@@ -78,11 +99,17 @@ end % for i_part
 tfG
 
 %% open linear system analyzer for each part
-i_part = part_menu();   % initial choice
+i_part = part_menu();       % initial choice
 while (i_part ~= -1)
     linearSystemAnalyzer(tfG{i_part, 1:n_tfG(i_part)})
     i_part = part_menu();   % ask again
 end % while ((i_part=part_menu()) ~= -1)
+
+%% show pole table
+pole_2nd_ord_table = cell2table(poles_0102_0104, 'VariableNames', [ ...
+    "tf a pole+" "tf a pole-" "tf b,d,f pole+" "tf b,d,f pole-" ...
+    "tf c,e,g pole+" "tf c,e,g pole-" ...
+])
 
 %% report done
 disp('Done.')
