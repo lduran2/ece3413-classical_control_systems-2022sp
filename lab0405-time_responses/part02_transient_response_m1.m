23b0f27272ae3b00ae1623f1cfe308573da5d998
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-03-22 22:33:32 -0400

    [lab0405] can new figure after each test

--- lab0405-time_responses/part02_transient_response_m1.m
+++ lab0405-time_responses/part02_transient_response_m1.m
@@ -2,11 +2,14 @@
 % Canonical : https://github.com/lduran2/ece3413_classical_control_systems/lab0405-time_responses/part02_transient_response_m1.m
 % The transient response plots and pole-zero plots of various systems.
 % By        : Leomar Duran <https://github.com/lduran2>
-% When      : 2022-03-22t22:11Q
+% When      : 2022-03-22t22:30Q
 % For       : ECE 3413
-% Version   : 1.5.1
+% Version   : 1.5.2
 %
 % CHANGELOG :
+%   v1.5.2 - 2022-03-22t22:30Q
+%       can new figure after each test
+%
 %   v1.5.1 - 2022-03-22t22:11Q
 %       switched param and test loops
 %
@@ -46,6 +49,9 @@ the_poles{3,:} = {-200, -20, -10, -2,   [],  [],  [], [], []}
 the_zeros{3,:} = {  [],  [],  [], [], -200, -50, -20, -5, -2}
 [~, N_PARAMS{3}] = size(the_poles{3,:})
 
+% whether to show a new figure after each test
+new_fig_per_test{3} = false
+
 %% G4(s; a, b) and G5(s; a, b) as G45(s; a, b; c)
 G_base45 = @(c) (@(a, b) zpk([-a], [-b; roots([1 c])], (c(end)*b/a)))
 
@@ -56,6 +62,8 @@ the_poles{4,:} = { 3.01, 3.1, 3.3, 3.5, 4.0 }
 the_zero{4} = 3
 [~, N_PARAMS{4}] = size(the_poles{4,:})
 the_zeros{4,:} = num2cell( the_zero{4}*ones(1, N_PARAMS{4}) )
+% whether to show a new figure after each response
+new_fig_per_test{4} = true
 
 % G5
 G_base{5} = G_base45([40 2500])
@@ -64,12 +72,12 @@ the_poles{5,:} = { 30.01, 30.1, 30.5, 31, 35, 40 }
 the_zero{5} = 30
 [~, N_PARAMS{5}] = size(the_poles{5,:})
 the_zeros{5,:} = num2cell( the_zero{5}*ones(1, N_PARAMS{5}) )
+% whether to show a new figure after each response
+new_fig_per_test{5} = true
 
 %% analysis
 % for each system
 for i_sys = sys_range
-    % new figure
-    fig = figure;
     % for each pole, zero
     for i_param=1:N_PARAMS{i_sys}
         % build the system
@@ -79,13 +87,23 @@ for i_sys = sys_range
     end % for i_param
     % for each test
     for i_test=1:N_STANDARD_TESTS
+        % make a new figure if first test, or if should after this test
+        if (new_fig_per_test{i_sys} || (i_test==1))
+            fig = figure;
+        end % if (new_fig_per_test{i_sys} || (i_test==1))
         % for each pole, zero
         for i_param=1:N_PARAMS{i_sys}
+            % calculate number of rows and row offset based on
+            % whether to make a new figure after each test
+            if (new_fig_per_test{i_sys})
+                N_ROWS = 1;
+                ROW_OFF = 0;
+            else
+                N_ROWS = N_STANDARD_TESTS;
+                ROW_OFF = N_PARAMS{i_sys}*(i_test - 1);
+            end % if (new_fig_per_test{i_sys})
             % start plotting
-            subplot( ...
-                N_STANDARD_TESTS, N_PARAMS{i_sys}, ...
-                (N_PARAMS{i_sys}*(i_test - 1) + i_param) ...
-            )
+            subplot(N_ROWS, N_PARAMS{i_sys}, (ROW_OFF + i_param))
             % impulse plot with an additional zero for each test
             impulseplot(G{i_sys,i_param}*zpk([], zeros(1,i_test), 1))
             % label the test signal response
@@ -97,12 +115,13 @@ for i_sys = sys_range
                 ylabel('')
             end % if (i_param~=1)
             % subtitle to label what zeros and poles on first row
-            if (i_test==1)
+            % include if it's a new figure
+            if (new_fig_per_test{i_sys} || (i_test==1))
                 subtitle(join([ ...
                     'z={' string(the_zeros{i_sys}{i_param}) '}' newline ...
                     'p={' string(the_poles{i_sys}{i_param}) '}' ...
                 ], ''))
-            end % if (i_test==1)
+            end % if (new_fig_per_test{i_sys} || (i_test==1))
         end % for i_param
     end % for i_test
 end % for i_sys
